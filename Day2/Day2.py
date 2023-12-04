4d1193caf53421ddcc439c2e57ae6e569c6522ba
Maddi <dawhat18@gmail.com>
2023-12-04 05:56:20 -0600

    Added Day 2
    Added day 2. Got part 1 working. Part 2 is still in progress

---
+++ Day2/Day2.py
@@ -0,0 +1,76 @@
+# Check for 12 red, 13 green, 14 blue
+
+with open('Day2/input.txt','r') as f:
+    data = f.readlines()
+
+redmax = 12
+greenmax = 13
+bluemax = 14
+
+possgametotal = 0
+smallestpossible = 0
+
+def pause():
+    input(">>")
+
+for line in data:
+    pullred = 0
+    pullgreen = 0
+    pullblue = 0
+
+    highestred = 0
+    highestgreen = 0
+    highestblue = 0
+    # Separate the game number from the data from each pull
+    game = line.strip().split(': ')
+    # The item 1 in the Game list contains all the game pulls
+    # by splitting it at the semi-colon, we are breaking each pull into a new list
+    pulls = game[1].split('; ')
+    # take the first item in the game list, remove everything up to the space which removes 'Game: '
+    # then we are left with just the number which we can then turn into an integer for use later
+    gamenum = int(game[0].strip().split(' ')[1])
+    print("Game Number: {}".format(gamenum))
+    # this happens for every pull in each game
+    possible = True
+    for pull in pulls:
+        pullred = 0
+        pullgreen = 0
+        pullblue = 0      
+
+        cubes = pull.strip().split(', ')
+        print("processing: {}".format(cubes))
+        for cube in cubes:
+            number = int(cube.strip().split(' ')[0])
+            color = cube.strip().split(' ')[1]
+            print("Color: {} | Number: {}".format(color,number))
+            match color:
+                case "blue":
+                    pullblue += number
+                    print("Case BLUE")
+                    if number > highestblue:
+                        highestblue = number
+                case "green":
+                    pullgreen += number
+                    print("Case GREEN")
+                    if number > highestgreen:
+                        highestgreen = number
+                case "red":
+                    pullred += number
+                    print("Case RED")
+                    if number > highestred:
+                        highestred = number
+            
+            if pullblue > bluemax or pullgreen > greenmax or pullred > redmax:
+                possible = False
+
+        if possible:
+            smallestpossible += (highestblue*highestgreen*highestred)
+    if possible:
+        possgametotal += gamenum
+        
+    #input(":::::")
+        
+    
+print(possgametotal)
+
+print(smallestpossible)
